fb581953687a0083148d78d98a599f606a0bd96b
ariel felippi <ariel.felippi@hotmail.com>
2023-09-12 21:57:52 -0300

    calendario

--- calendario.php
+++ calendario.php
@@ -16,25 +16,50 @@ $dia = (int) $data[0];
 $mes = (int) $data[1];
 $ano = (int) $data[2]; // '1990' => 1990
 
-// 01 || 1 = mesmo.
+$meses30dias = [4, 6, 9, 11];
+$meses31dias = [1, 3, 5, 7, 8, 10, 12];
 
+function isBissexto($ano) {
+    if ( ($ano % 4 == 0) && ($ano % 100 != 0 || $ano % 400 == 0) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// ajuste para dias invalidos
 if ($dia < 1 || $dia > 31) {
     $dia = 1;
 }
 
+// ajuste para meses invalidos
 if ($mes < 1 || $mes > 12) {
     $mes = 1;
 }
 
-$meses30dias = [4, 6, 9, 11];
-$meses31dias = [1, 3, 5, 7, 8, 10, 12];
+if ($mes == 2 && $dia >= 28) {
+    $dia = 28;
+
+    if (isBissexto($ano)) {
+        $dia = 29;
+    }
+}
 
 if (in_array($mes, $meses30dias) && $dia > 30 ) {
     $dia = 1;
     $mes += 1;
 }
 
+// restante da logica aqui
 
+$bissextoStr = "O ano {$ano} NÃO é bissexto.";
 
+if (isBissexto($ano)) {
+    $bissextoStr = "O ano {$ano} é bissexto.";
+}
 
+// 32/05/1990 => 01/06/1990 (Corrigi para uma data valida).
+$dataStr = "{$dia}/{$mes}/{$ano}"; // 01/06/1990
 
+echo "A data informada {$dataStr}. {$bissextoStr}";
+// A data informada 01/06/1990. O ano 1990 NÃO é bissexto.
